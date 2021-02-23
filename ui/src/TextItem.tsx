84ec4743c2e146c31c681da4d430df271226cf1c
Uwe Riegel <uriegel@hotmail.de>
2021-02-23 14:47:24 +0100

    Restriction first step

--- ui/src/TextItem.tsx
+++ ui/src/TextItem.tsx
@@ -6,52 +6,63 @@ export type TextItemProps = {
     restrictions: string[]
 }
 
+type restriction = {
+    item: string,
+    index: number
+}
+
 export const TextItem = ({item, restrictions }: TextItemProps) => {
+    
+    const renderRestricted = () => {
+        function getRestricted(itemToRestrict: restriction, res: restriction) {
+            const seplen = res.item.length
+            // get next not highlighted part, and highlighted part
+            function* getParts() {
+                let index = 0
+                while (true) {
+                    const pos = itemToRestrict.item.toLowerCase().indexOf(res.item.toLowerCase(), index)
+                    yield pos != -1
+                        ? { 
+                            item: itemToRestrict.item.substr(index, pos - index), 
+                            sep: itemToRestrict.item.substr(pos - index, seplen) 
+                        }
+                        : { 
+                            item: itemToRestrict.item.substr(index), 
+                            sep: "" 
+                        }
+                    if (pos == -1)
+                        break                            
+                    index = pos + seplen
+                }
+            }
+            function getHighlighted() {
+                let result = ""
+                const parts = Array.from(getParts())
+                parts.forEach(n => {
+                    result += n.item
+                    if (n.sep.length > 0)
+                        result += `<span className='highlight${itemToRestrict.index}'>${n.sep}</span>`
+                })
+                return result;
+            }
+            return { 
+                item: getHighlighted(), 
+                index: itemToRestrict.index <= 3 ? itemToRestrict.index + 1 : 4
+            }
+        }
+    
+        const result = restrictions
+            .map((n, i) => ({ item: n, index: i }))
+            .reduce((acc, res) => getRestricted(acc, res), { item: item.item, index: 1 })
+
+        return <td className='textitem' key={1}>{result.item}</td> 
+    }
+
     return restrictions.length == 0
         ?  <td className='textitem' key={1}>{item.item}</td> 
-        :  <td className='textitem' key={1}> Schön {item.item}</td> 
+        :  renderRestricted()
 }
 
 
 
-// const getRestricted = item => {
-//     function getRestricted(itemToRestrict, res) {
-//         const seplen = res.length
-//         // get next not highlighted part, and highlighted part
-//         function* getParts() {
-//             let index = 0
-//             while (true) {
-//                 const pos = itemToRestrict.item.toLowerCase().indexOf(res.toLowerCase(), index)
-//                 yield pos != -1
-//                     ? { 
-//                         item: itemToRestrict.item.substr(index, pos - index), 
-//                         sep: itemToRestrict.item.substr(pos - index, seplen) 
-//                     }
-//                     : { 
-//                         item: itemToRestrict.item.substr(index), 
-//                         sep: "" 
-//                     }
-//                 if (pos == -1)
-//                     break                            
-//                 index = pos + seplen
-//             }
-//         }
-//         function getHighlighted() {
-//             let result = ""
-//             const parts = Array.from(getParts())
-//             parts.forEach(n => {
-//                 result += n.item
-//                 if (n.sep.length > 0)
-//                     result += `<span class='event-log-vue-selected${itemToRestrict.index}'>${n.sep}</span>`
-//             })
-//             return result;
-//         }
-//         return { 
-//             item: getHighlighted(), 
-//             index: itemToRestrict.index <= 3 ? itemToRestrict.index + 1 : 4
-//         }
-//     }
     
-//     const result = this.restrictions.reduce((acc, res) => getRestricted(acc, res), { item, index: 1 })
-//     return result.item
-// }
